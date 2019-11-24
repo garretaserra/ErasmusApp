d516e23313221687c5ea40e8a15ad2072f3787df
carlogattuso <carlogc46@gmail.com>
2019-11-24 20:13:18 +0100

    Name validation done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./views/home/home.module').then(m => m.HomePageModule)},
   {
     path: 'register',
--- src/app/views/auth/register/register.page.ts
+++ src/app/views/auth/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,44 @@ import { AuthService } from "../auth.service";
 })
 export class RegisterPage implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  registerForm: FormGroup;
+  validation_messages: any;
 
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^[A-Z]+(([',. -][A-Z ])?[a-zA-Z]*)*$/),
+        Validators.minLength(2)])),
+      /*email: new FormControl('', Validators.compose([
+        Validators.required ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required ])),
+      confirm: new FormControl('', Validators.compose([
+        Validators.required ])),*/
+    });
   }
 
-  register(form){
-    this.authService.register(form.value);
+  ngOnInit() {
+    this.validation_messages = {
+      'name': [
+        { type: 'required', message: 'Campo obligatorio' },
+        { type: 'pattern', message: 'Debe empezar con mayúsculas y no contener números' },
+        { type: 'minlength', message: 'Demasiado corto'}
+      ]
+      /*'email': [
+        { type: 'required', message: 'Campo obligatorio.' }
+      ],
+      'password': [
+        { type: 'required', message: 'Campo obligatorio.' }
+      ],
+      'confirm': [
+        { type: 'required', message: 'Campo obligatorio.' }
+      ],*/
+    }
+  }
+
+  register(){
+    this.authService.register(this.registerForm.value);
   }
 }
