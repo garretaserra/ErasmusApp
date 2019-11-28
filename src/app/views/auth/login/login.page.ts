23acd774bfd0c19551a45aefef9d1ff69bd8fce0
Marioboto3 <marioboto98@gmail.com>
2019-11-28 22:48:56 +0100

    Login with validations.

--- src/app/views/auth/login/login.page.ts
+++ src/app/views/auth/login/login.page.ts
@@ -15,17 +15,27 @@ export class LoginPage implements OnInit {
   validation_messages: any;
 
   constructor(private authService: AuthService, private router: Router,  private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)])),
+      password: new FormControl()
+    });
   }
 
-  ngOnInit() {
-    this.loginForm = new FormGroup({email: new FormControl(), password: new FormControl()});
+  ngOnInit() { this.validation_messages = {
+    'email': [
+      { type: 'required', message: 'Campo obligatorio.' },
+      { type: 'pattern', message: 'Debe ser un correo electrónico válido'}
+    ]
+  };
   }
 
   login() {
-    console.log('email: ', this.loginForm.controls['email'].value);
-    console.log('email: ', this.loginForm.controls['password'].value);
-    this.authService.login(new UserLogin( this.loginForm.controls['email'].value,
-        this.loginForm.controls['password'].value)).subscribe(res => {
+    console.log('email: ', this.loginForm.controls.email.value);
+    console.log('email: ', this.loginForm.controls.password.value);
+    this.authService.login(new UserLogin( this.loginForm.controls.email.value,
+        this.loginForm.controls.password.value)).subscribe(res => {
           console.log(res);
         },
         err => {
