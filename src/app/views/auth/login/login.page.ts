1e84843fbc037ed6a6b8edf69e6b072563e497b8
carlogattuso <carlogc46@gmail.com>
2019-11-28 16:56:40 +0100

    register responsive working

--- src/app/views/auth/login/login.page.ts
+++ src/app/views/auth/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService} from '../auth.service';
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../user";
 
 @Component({
   selector: 'app-login',
@@ -9,11 +11,13 @@ import { AuthService} from '../auth.service';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
-  login(form) {
+  login() {
   }
 }
