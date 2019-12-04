2b5cb5171d23c8cf78f9ddcb99362f10d0b475a9
Marioboto3 <marioboto98@gmail.com>
2019-12-04 20:44:45 +0100

    First view of Home Page.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import {AngularMaterialModule} from '../../../../../ErasmusWeb/src/app/angular-material.module';
+
+
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: HomePage
+            }
+        ]),
+        ReactiveFormsModule,
+        AngularMaterialModule,
+    ],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {MenuController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  homeForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, public menuCtrl: MenuController) {}
+
+  ngOnInit() {
+    this.homeForm = this.formBuilder.group({
+      email: new FormControl()
+    });
+  }
+  openMenu() {
+    this.menuCtrl.open();
+  }
+  login() { document.querySelector('ion-menu-controller')
+      .open(); }
 }
