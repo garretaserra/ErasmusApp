8bc332b6f8115bb803bd9b345944600ac095df0c
Izan Pérez Cosano <izanperez98@gmail.com>
2020-01-18 13:30:41 +0100

    get events from user

--- src/app/views/globe/globe.page.ts
+++ src/app/views/globe/globe.page.ts
@@ -1,5 +1,10 @@
 import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
 import * as mapBox from 'mapbox-gl';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {StorageComponent} from '../../storage/storage.component';
+import {User} from '../../models/User/user';
+import {Event} from '../../models/Event/event';
 
 @Component({
   selector: 'app-globe',
@@ -9,14 +14,20 @@ import * as mapBox from 'mapbox-gl';
 export class GlobePage implements OnInit, AfterContentInit {
 
   map: mapBox.Map;
+  eventList: Event[];
+  user: User;
 
   @ViewChild('mapElement', {static: true}) mapElement;
 
-  constructor() {
+  constructor(public http: HttpClient, public storage: StorageComponent) {
     mapBox.accessToken = 'pk.eyJ1IjoiaGFubGV5d29vZCIsImEiOiJZcVlldnlRIn0.BHYD98R8UQQoUBLsNd8ksg';
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+      this.user = JSON.parse(this.storage.getUser());
+      console.log(this.user._id);
+
+      console.log(this.eventList);
   }
 
   ngAfterContentInit(): void {
@@ -32,6 +43,26 @@ export class GlobePage implements OnInit, AfterContentInit {
       zoom: 12,
       // scrollZoom: false
     });
+    this.http.get<Event[]>(environment.apiUri + '/user/events/' + this.user._id).toPromise().then((data) => {
+        data.forEach((item: Event) => {
+            const el = document.createElement('img');
+            el.src = '../../../assets/img/default_user.png';
+            el.className = 'marker';
+            const long = item.location.split(',')[0];
+            const lat = item.location.split(',')[1];
+            new this.map.Marker(el, {offset: [0, -23]})
+                .setLngLat(long, lat)
+                .addTo(this.mapElement);
+        });
+    });
+    /*
+    this.mapElement.on('load', () => {
+      // This is where your '.addLayer()' used to be, instead add only the source without styling a layer
+      this.mapElement.addSource('places', this.eventList);
+      // Initialize the list
+      buildLocationList(stores);
+
+    });*/
   }
 }
 
