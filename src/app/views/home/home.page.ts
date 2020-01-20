6999f3025af20a20694566e2b6ed1c0c2c21afa1
Izan Pérez Cosano <izanperez98@gmail.com>
2020-01-20 12:42:26 +0100

    added chatglobal

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -77,16 +77,18 @@ export class HomePage implements OnInit {
             this.following = (await this.homeService.getFollowing(this.user._id).toPromise()).following;
             this.getActivity();
             this.chatService.connectSocket(this.user.name);
-            this.chatService.getMessage().subscribe((data: {message, email}) => {
-                console.log(data);
-                const goToUrl = '/conversation/' + data.email;
-                const msg = data.email + ' says: ' + data.message;
-                if (this.router.url !== goToUrl) {
-                    this.notificationComponent.generateToast(msg, goToUrl).catch((err) => console.log(err));
-                } else {
-                    this.notificationComponent.playInnerSound();
+            this.chatService.getMessage().subscribe((data: {message, email, everyone}) => {
+                console.log('true?', data.everyone);
+                if (data.everyone !== true) {
+                    console.log(data);
+                    const goToUrl = '/conversation/' + data.email;
+                    const msg = data.email + ' says: ' + data.message;
+                    if (this.router.url !== goToUrl) {
+                        this.notificationComponent.generateToast(msg, goToUrl).catch((err) => console.log(err));
+                    } else {
+                        this.notificationComponent.playInnerSound();
+                    }
                 }
-                console.log(this.router.url);
             });
         }
     }
